ca44830a31456e5aa7c4fa24bbbb51171f525e12
zenakmodul14 <zenakorneev940@gmail.com>
2022-05-23 15:11:41 +0300

    6 задание готово

--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+def calculation_formula(x, y, r):
+    if r > x and r > y:
+        print('Монетка где-то рядом')
+    else:
+        print('Монетки в области нет')
+
+
+calculation_formula(x=float(input('X: ')), y=float(input('Y: ')), r=float(input('Введите радиус: ')))
