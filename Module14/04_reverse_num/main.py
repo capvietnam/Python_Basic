1a38be86e429303ad4a77e895d8d780ac066cc36
zenakmodul14 <zenakorneev940@gmail.com>
2022-05-23 14:41:42 +0300

    4 задание готово

--- Module14/04_reverse_num/main.py
+++ Module14/04_reverse_num/main.py
@@ -1,3 +1,18 @@
-# TODO здесь писать код
+def coup(list):
+    number3 = ''
+    for i in str(list[::-1]):
+        number3 += i
+    return number3
 
 
+def work_in_parts(number):
+    list = str(number).split('.')
+    return float(coup(list=list[0]) + '.' + coup(list=list[1]))
+
+
+number1 = work_in_parts(number=input('Введите первое число: '))
+number2 = work_in_parts(number=input('Введите второе число: '))
+
+print('Первое число наоборот: ', number1)
+print('Второе число наоборот: ', number2)
+print('Сумма: ', number1 + number2)
