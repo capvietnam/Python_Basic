de7941547e0ce34304e941a15aa6cf19168bb5b8
zenakmodul14 <zenakorneev940@gmail.com>
2022-05-23 13:17:10 +0300

    3 задание готово

--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+def summ(number):
+    summ_number = 0
+    for i in number:
+        summ_number += int(i)
+    return summ_number
+
+def quantity(number):
+    quantity_number = 0
+    for i in number:
+        quantity_number += 1
+    return quantity_number
+
+number = input('Введите число: ')
+summ = summ(number=number)
+quantity = quantity(number=number)
+
+print('Сумма чисел:', summ)
+print('Количество цифр в числе:', quantity)
+print('Разность суммы и количества цифр:', summ - quantity)
