1e797f8c42f7ffb5fab9651d91c2de0cc60ed3a1
zenakmodul14 <zenakorneev940@gmail.com>
2022-05-23 14:54:41 +0300

    5 задание готово

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,10 @@
 # TODO здесь писать код
+
+def smallest_divisor(number):
+    for i in range(2, number + 1):
+        if number % i == 0:
+            print('Наименьший делитель, отличный от единицы:', i)
+            break
+
+
+number = smallest_divisor(number=int(input('Введите число: ')))
