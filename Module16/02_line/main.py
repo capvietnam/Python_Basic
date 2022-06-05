8bf4d5581f39a75b5fa9e4e3b1eb57995b3b6899
zenakmodul14 <zenakorneev940@gmail.com>
2022-06-05 22:22:25 +0300

    3 задание готово

--- Module16/02_line/main.py
+++ Module16/02_line/main.py
@@ -1,1 +1,6 @@
-# TODO здесь писать код
+first_shirinka = list(range(160, 173, 2))
+second_shirinka = list(range(162, 181, 3))
+
+first_shirinka.extend(second_shirinka)
+first_shirinka.sort()
+print(first_shirinka)
