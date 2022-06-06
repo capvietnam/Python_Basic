d235e80e68235c1391284838a0db58de01df12b2
zenakmodul14 <zenakorneev940@gmail.com>
2022-06-06 13:52:43 +0300

    10 задание готово

--- Module16/10_simmetrical_seq/main.py
+++ Module16/10_simmetrical_seq/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+number = int(input('Кол-во чисел: '))
+list_number = []
+
+for i in range(number):
+    list_number.append(int(input('Число: ')))
+
+counter = 0
+while list_number != list_number[::-1]:
+    list_number.append(list_number[counter])
+    counter += 1
+
+print('Нужно приписать чисел:', counter)
+print('Сами числа:', list_number[:counter][::-1])
