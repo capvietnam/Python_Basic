79f8b13b5886b46d068e6639eb079474ad9e234d
zenakmodul14 <zenakorneev940@gmail.com>
2022-06-08 01:18:01 +0300

    6 задание

--- Module17/06_list_compression/main.py
+++ Module17/06_list_compression/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+import random
+
+
+list_number = [random.randint(0, 2) for _ in range(int(input('Количество чисел в списке: ')))]
+
+print('Список до сжатия:', list_number)
+
+list(list_number.remove(i) for i in list_number[:] if i == 0)
+print('Список после сжатия:', list_number)
