bea97f0f6127ffbbd895356de826d8e800ab36d1
zenakmodul14 <zenakorneev940@gmail.com>
2022-06-07 20:43:40 +0300

    1 задание

--- Module17/01_vowels/main.py
+++ Module17/01_vowels/main.py
@@ -1,1 +1,11 @@
 # TODO здесь писать код
+
+text = input('Введите текст: ')
+list_symbols = []
+vowels = ['а', 'о', 'э', 'е', 'и', 'ы', 'у', 'ё', 'ю', 'я']
+for i in text:
+    if i in vowels:
+        list_symbols.append(i)
+
+print('Список гласных букв:', list_symbols)
+print('Длина списка:', len(list_symbols))
