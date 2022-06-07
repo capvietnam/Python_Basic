99c30a624bb38b5b0f7279617e080faaea69e3fa
zenakmodul14 <zenakorneev940@gmail.com>
2022-06-07 21:18:32 +0300

    3 задание

--- Module17/03_random_competition/main.py
+++ Module17/03_random_competition/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+import random
+
+list1 = [round(random.uniform(5, 10), 2) for i in range(20)]
+list2 = [round(random.uniform(5, 10), 2) for t in range(20)]
+
+print(list1)
+print(list2)
+
+print([list1[s] if list1[s] > list2[s] else list2[s] for s in range(20)])
