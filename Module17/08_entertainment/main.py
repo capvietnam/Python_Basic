38af43222e0a7d8ca14c5b38764910eb99d78bde
zenakmodul14 <zenakorneev940@gmail.com>
2022-06-08 14:19:02 +0300

    8 задание

--- Module17/08_entertainment/main.py
+++ Module17/08_entertainment/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+from random import randint
+list_sticks = [i + 1 for i in range(int(input('Количество палок: ')))]
+print(list_sticks)
+throws = int(input('Количество бросков: '))
+answer = ''
+
+for throw in range(throws):
+    numbers = randint(1, len(list_sticks) - 3)
+    print(f'Бросок {throw + 1}. Сбиты палки с номера {numbers} по номер {numbers + 3}')
+    numbers_list = list(range(numbers, numbers + 4))
+    list_sticks = ['.' if t in numbers_list or t == '.' else t for t in list_sticks]
+
+list_sticks = ['I' if s != '.' else '.' for s in list_sticks]
+
+print('Результат: ' + ''.join(list_sticks))
