5ce14532270b6adc2e1fe0a15fcd4bc604b865e3
zenakmodul14 <zenakorneev940@gmail.com>
2022-06-10 01:46:49 +0300

    4 задание готово

--- Module18/04_capital_letters/main.py
+++ Module18/04_capital_letters/main.py
@@ -1,1 +1,6 @@
-# TODO здесь писать код
+string_list = input('Введите строку: ').split()
+
+for i in string_list:
+    string_list[string_list.index(i)] = i[0].upper() + i[1:]
+
+print(' '.join(string_list))
