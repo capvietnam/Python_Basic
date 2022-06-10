c229a6d0418b759ea9961c5debaaabb6108b5c05
zenakmodul14 <zenakorneev940@gmail.com>
2022-06-10 10:46:10 +0300

    7 задание готово

--- Module18/07_ip_adress_2/main.py
+++ Module18/07_ip_adress_2/main.py
@@ -1,1 +1,20 @@
 # TODO здесь писать код
+while True:
+    list_number_ip = input('Введите IP: ').split('.')
+    if len(list_number_ip) != 4:
+        print('Адрес — это четыре числа, разделённые точками.')
+    else:
+        break
+
+for number in list_number_ip:
+    if number.isnumeric() == False:
+        print(f'{number} — это не целое число.')
+        break
+    elif int(number) >  255:
+        print(f'{number} превышает 255.')
+        break
+    elif 0 > int(number):
+        print(f'{number} меньше 0.')
+        break
+else:
+    print('IP-адрес корректен.')
