5834e8e53fc37631399a3db990dd7b87b4336186
zenakmodul14 <zenakorneev940@gmail.com>
2022-06-10 12:45:26 +0300

    6 задание готово

--- Module18/06_compression/main.py
+++ Module18/06_compression/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+text2 = ''
+text = input('Введите строку: ') + ' '
+retr_symbol = text[0]
+quantity = 0
+for symbol in text:
+    if retr_symbol != symbol:
+        text2 += retr_symbol + str(quantity)
+        retr_symbol = symbol
+        quantity = 0
+    quantity += 1
+print('Закодированная строка:', text2)
