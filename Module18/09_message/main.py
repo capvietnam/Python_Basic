5221c52c268151f7cb98f60deab848e28a2766ff
zenakmodul14 <zenakorneev940@gmail.com>
2022-06-13 00:23:22 +0300

    modul 18 9 задание финал

--- Module18/09_message/main.py
+++ Module18/09_message/main.py
@@ -2,18 +2,19 @@
 # list_text = list(i[::-1] if i.isalpha() else i[-2::-1] + i[-1] for i in input('Сообщение: ').split())
 #
 # print('Новое сообщение: ' + ' '.join(list_text))
+dict_word = dict()
 True_text = ''
 isl_text = ''
 text = input('Сообщение: ').split()
 for i in text:
-    if i[1:-2].isalpha() == False:
+    if i[1:-2].isalpha() == False and True in list(k.isalpha() for k in i):
         for s in i:
             if s.isalpha() == False:
                 t = i.split(s)
                 True_text = t[0][::-1] + s + t[1][::-1]
-                isl_text = i
+                dict_word[i] = True_text
 
 list_text = list(i[::-1] if i.isalpha()
-                 else True_text if i == isl_text else i[-2::-1] + i[-1] for i in text)
+                 else dict_word.setdefault(i) if i in dict_word.keys() else i[-2::-1] + i[-1] for i in text)
 
 print('Новое сообщение: ' + ' '.join(list_text))
