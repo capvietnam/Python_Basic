e04dfb52ecea8cd20b670426143a754c9e829fc2
zenakmodul14 <zenakorneev940@gmail.com>
2022-06-13 00:23:22 +0300

    modul 18 9 задание финал

--- Module18/09_message/main.py
+++ Module18/09_message/main.py
@@ -1,19 +1,21 @@
-#
-# list_text = list(i[::-1] if i.isalpha() else i[-2::-1] + i[-1] for i in input('Сообщение: ').split())
-#
-# print('Новое сообщение: ' + ' '.join(list_text))
+dict_word = dict()
 True_text = ''
 isl_text = ''
 text = input('Сообщение: ').split()
+
+
+""""Ищем где внутри слов есть символы (отличные от букв) и переворачиваем относительно символа"""
 for i in text:
-    if i[1:-2].isalpha() == False:
+    if i[0:-2].isalpha() == False and True in list(k.isalpha() for k in i):
         for s in i:
             if s.isalpha() == False:
                 t = i.split(s)
                 True_text = t[0][::-1] + s + t[1][::-1]
-                isl_text = i
+                dict_word[i] = True_text
 
-list_text = list(i[::-1] if i.isalpha()
-                 else True_text if i == isl_text else i[-2::-1] + i[-1] for i in text)
+
+""""Создаем список со значениями и переворачиваем без учета символа"""
+list_text = list(f[::-1] if f.isalpha()
+                 else dict_word.setdefault(f) if f in dict_word.keys() else f[-2::-1] + f[-1] for f in text)
 
 print('Новое сообщение: ' + ' '.join(list_text))
