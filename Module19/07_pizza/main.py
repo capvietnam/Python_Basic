0ca3b57ed384bd917335eb2a91d5dc0880199ac0
zenakmodul14 <zenakorneev940@gmail.com>
2022-06-14 12:42:05 +0300

    modul 19 7 задание готово

--- Module19/07_pizza/main.py
+++ Module19/07_pizza/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+quantity = int(input('Введите количество заказов: '))
+orders_data = dict()
+
+for i in range(1, quantity + 1):
+    name, pizza, amount = input(f'{i} заказ: ').split()
+    amount = int(amount)
+    if name not in orders_data:
+        orders_data[name] = {pizza: amount}
+    else:
+        if pizza not in orders_data[name]:
+            orders_data[name][pizza] = amount
+        else:
+            orders_data[name][pizza] += amount
+
+for fio, order in sorted(orders_data.items()):
+    print(f'{fio}:')
+    for pizza, amount in sorted(order.items()):
+        print('\t', pizza, amount)
