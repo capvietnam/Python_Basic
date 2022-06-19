c48df3893682e95cdeea6e167a981a24dc20f9ce
zenakmodul14 <zenakorneev940@gmail.com>
2022-06-19 22:10:55 +0300

    5 задание готово

--- Module20/05_one_family/main.py
+++ Module20/05_one_family/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+dict_base = {('Сидоров', 'Никита'): 35, ('Сидорова', 'Алина'): 34, ('Сидоров', 'Павел'): 10}
+
+scan = input('Введите фамилию: ').title()
+
+for key, value in dict_base.items():
+    if scan or scan+'а' or scan[:-2] in key:
+        print(key[0], key[1], value)
