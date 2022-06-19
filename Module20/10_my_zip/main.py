639a8d279705ae9230b0515e93b9140a9ed77ae7
zenakmodul14 <zenakorneev940@gmail.com>
2022-06-20 02:02:34 +0300

    10 задание готово

--- Module20/10_my_zip/main.py
+++ Module20/10_my_zip/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+import re
+
+
+stroka = input('Строка: ')
+corteg = tuple(int(i) for i in re.findall(r'-?\d+\.?\d*', input('Кортеж чисел: ')))
+zip_new = ((stroka[i], corteg[i]) for i in range(min(len(corteg), len(stroka))))
+print(zip_new)
+for t in zip_new:
+    print(t)
