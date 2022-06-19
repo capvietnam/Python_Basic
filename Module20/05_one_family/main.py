986d96a24781c857271156acf6fd4ab75b8d4006
zenakmodul14 <zenakorneev940@gmail.com>
2022-06-19 22:46:44 +0300

    7 задание готово

--- Module20/05_one_family/main.py
+++ Module20/05_one_family/main.py
@@ -1,7 +1,6 @@
 dict_base = {('Сидоров', 'Никита'): 35, ('Сидорова', 'Алина'): 34, ('Сидоров', 'Павел'): 10}
 
 scan = input('Введите фамилию: ').title()
-
 for key, value in dict_base.items():
-    if scan or scan+'а' or scan[:-2] in key:
+    if scan in key or scan+'а' in key or scan[:-1] in key:
         print(key[0], key[1], value)
--- Module20/07_sort_function/main.py
+++ Module20/07_sort_function/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+def tpl_sort(sort):
+    for i in sort:
+        if type(i) != int:
+            return sort
+    return tuple(sorted(sort))
+
+print(tpl_sort((6, 3, -1, 8, 4, 10, -5)))
