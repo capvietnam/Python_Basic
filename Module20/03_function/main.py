b32e3c74b0e16d4a4a361eac3cea7dce825eea65
zenakmodul14 <zenakorneev940@gmail.com>
2022-06-21 00:43:21 +0300

    3 задание готово

--- Module20/03_function/main.py
+++ Module20/03_function/main.py
@@ -1,14 +1,12 @@
-from collections import Counter
-
 def slicer(check, symbol):
     new_list = list()
-    qyantity = 0
+    quantity = 0
     for i in check:
         if i == symbol:
-            qyantity += 1
-        if qyantity == 1:
+            quantity += 1
+        if quantity == 1:
             new_list.append(i)
-    if qyantity > 1:
+    if quantity > 1:
         new_list.append(symbol)
     return tuple(new_list)
 
