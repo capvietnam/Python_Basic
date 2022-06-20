33242c59f74ddbe5a41c8b4e49df669e701bb0b4
zenakmodul14 <zenakorneev940@gmail.com>
2022-06-21 00:31:30 +0300

    3 задание готово

--- Module20/03_function/main.py
+++ Module20/03_function/main.py
@@ -1,15 +1,26 @@
+from collections import Counter
+
 def slicer(check, symbol):
     list_check = list(check)
     list_ansver = list()
-    for i in check:
-        if i == symbol:
-            list_ansver.append(i)
-            for t in list_check[1:]:
-                list_ansver.append(t)
-                if list_ansver[-1] == symbol:
-                    return tuple(list_ansver)
-        list_check.remove(i)
+    if symbol not in list_check:
+        return ()
+    elif Counter(list_check)[symbol] == 1:
+        while True:
+            if list_check[0] == symbol:
+                return tuple(list_check)
+            else:
+                list_check.pop(0)
+    else:
+        for i in check:
+            if i == symbol:
+                list_ansver.append(i)
+                for t in list_check[1:]:
+                    list_ansver.append(t)
+                    if list_ansver[-1] == symbol:
+                        return tuple(list_ansver)
+            list_check.remove(i)
 
 
 
-print(slicer((1, 2, 3, 4, 5, 6, 7, 8, 2, 2, 9, 10), 2))
+print(slicer((1, 2, 3, 4, 5, 6, 7, 8, 9, 10), 2))
