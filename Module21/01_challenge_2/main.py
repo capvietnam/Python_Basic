74a974ab94491c6ec098179ddb2645dc58a42343
zenakmodul14 <zenakorneev940@gmail.com>
2022-06-22 00:26:54 +0300

    2 заданте готово

--- Module21/01_challenge_2/main.py
+++ Module21/01_challenge_2/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+num = 0
+def print_number(number):
+    global num
+    if number == num:
+        return number
+    else:
+        num += 1
+        print(num)
+        return print_number(number)
+
+
+print_number(10)
