ad50c0e5ae3d0b2245c25d89e06503f559e20d92
zenakmodul14 <zenakorneev940@gmail.com>
2022-06-22 00:42:20 +0300

    3 задание готово

--- Module21/03_fibonacci/main.py
+++ Module21/03_fibonacci/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+last1 = 1
+last2 = 1
+number = 2
+num_pos = int(input('Введите позицию числа в ряде Фибоначчи: '))
+while True:
+    last1, last2 = last2, last1 + last2
+    number += 1
+    if number == num_pos:
+        print('Число', last2)
+
+
+
+
