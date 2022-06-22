675c304e61f860b6f789cb78d49fa76007c8d682
zenakmodul14 <zenakorneev940@gmail.com>
2022-06-22 11:40:42 +0300

    4 задание готово

--- Module21/04_search_element_2/main.py
+++ Module21/04_search_element_2/main.py
@@ -1,1 +1,37 @@
-# TODO здесь писать код
+site = {
+    'html': {
+        'head': {
+            'title': 'Мой сайт'},
+        'body': {
+            'h2': 'Здесь будет мой заголовок',
+            'div': 'Тут, наверное, какой-то блок',
+            'p': 'А вот здесь новый абзац'
+        }
+    }
+}
+
+def seach(seach_key, depth):
+    if depth == 'y':
+        max_depth = int(input('Введите максимальную глубину: '))
+    else:
+        max_depth = None
+    for key in site.keys():
+        if key == seach_key:
+            return site[key]
+        elif max_depth == 1:
+            return None
+        for key1 in site[key]:
+            if key1 == seach_key:
+                return site[key][key1]
+            elif max_depth == 2:
+                return None
+            for key2 in site[key]:
+                if key2 == seach_key:
+                    return site[key][key1][key2]
+                else:
+                    return None
+
+
+print('Значение ключа:', seach(seach_key=input('Введите искомый ключ: '),
+      depth=input('Хотите ввести максимальную глубину? Y/N: ').lower())
+)
