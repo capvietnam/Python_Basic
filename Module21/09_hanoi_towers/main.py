d2d1697491d2d4fed3deaf6553f02a889a64dba7
zenakmodul14 <zenakorneev940@gmail.com>
2022-06-22 20:25:10 +0300

    9 задание готово

--- Module21/09_hanoi_towers/main.py
+++ Module21/09_hanoi_towers/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+# def move(n, start=1, finish=3):
+#     if n == 1:
+#         print(f"Переложить диск 1 со стержня номер {start} на стержень номер {finish}")
+#     else:
+#         move(n - 1, start, 6 - start - finish)
+#         print(f"Переложить диск {n} со стержня номер {start} на стержень номер {finish}")
+#         move(n - 1, 6 - start - finish, finish)
+
+def move(n, start=1, finish=3):
+     if n <= 0:
+         return
+     temp = 6 - start - finish
+     move(n-1, finish=temp)
+     print(f"Переложить диск {n} со стержня номер {start} на стержень номер {finish}")
+     move(n-1, start=temp)
+
+move(int(input("Введите кол-во дисков: ")))
