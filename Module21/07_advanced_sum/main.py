8b5cf96bcd6c027b9b9776b8aad7a970e4bdb21b
zenakmodul14 <zenakorneev940@gmail.com>
2022-06-22 14:52:22 +0300

    7 задание готово

--- Module21/07_advanced_sum/main.py
+++ Module21/07_advanced_sum/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+def sum(*array1):
+    summ = 0
+    array = [ar for ar in array1]
+    new_array = array[:]
+    ind = 0
+    while True:
+        try:
+            while isinstance(new_array[ind], list):
+                item = new_array.pop(ind)
+                for inner_item in reversed(item):
+                    new_array.insert(ind, inner_item)
+            ind += 1
+        except IndexError:
+            break
+    for i in new_array:
+        summ += i
+    return summ
+
+
+
+print(sum([[1, 2, [3]], [1], 3]))
+print(sum(1, 2, 3, 4, 5))
