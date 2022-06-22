e1e5ca321141ddf775bf540a471e0e9a92db40c1
zenakmodul14 <zenakorneev940@gmail.com>
2022-06-22 22:36:56 +0300

    6 задание улучшено

--- Module21/06_deep_copy/main.py
+++ Module21/06_deep_copy/main.py
@@ -1,3 +1,5 @@
+from pprint import pformat
+from yapf.yapflib.yapf_api import FormatCode
 site = {
     'html': {
         'head': {
@@ -11,12 +13,22 @@ site = {
     }
 }
 quantity = int(input('Сколько сайтов: '))
+
+def pretty(d, indent=0):
+   for key, value in d.items():
+      print('\t' * indent + '\t\t' + "'"+str(key)+"':")
+      if isinstance(value, dict):
+         pretty(value, indent+1)
+      else:
+         print('\t' * (indent+1) + '\t\t' + "'"+str(value)+"'")
+
 def new_site(new_prod):
     global quantity
     quantity -= 1
     site['html']['head']['title'] = f'Куплю/продам {new_prod} недорого'
     site['html']['body']['h2'] = f'У нас самая низкая цена на {new_prod}'
-    print(f'Сайт для {new_prod}:\n', 'site =', site)
+    formatted_code, _ = FormatCode(pformat(site))
+    print(f'Сайт для {new_prod}:\n', "site =", formatted_code)
     if quantity == 0:
         return
     new_site(new_prod=input('Введите название продукта для нового сайта: '))
