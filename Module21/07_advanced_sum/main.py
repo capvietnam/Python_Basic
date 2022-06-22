61c73a37947af82f3829171cc7d15eb081ca1a55
zenakmodul14 <zenakorneev940@gmail.com>
2022-06-22 22:40:40 +0300

    7 задание улучшено

--- Module21/07_advanced_sum/main.py
+++ Module21/07_advanced_sum/main.py
@@ -1,22 +1,19 @@
+def unification(nice_list):
+    for i in nice_list:
+        if type(i) == int:
+            new_list.append(i)
+        else:
+            unification(i)
+    return new_list
+
 def sum(*array1):
     summ = 0
-    array = [ar for ar in array1]
-    new_array = array[:]
-    ind = 0
-    while True:
-        try:
-            while isinstance(new_array[ind], list):
-                item = new_array.pop(ind)
-                for inner_item in reversed(item):
-                    new_array.insert(ind, inner_item)
-            ind += 1
-        except IndexError:
-            break
+    new_array = unification(array1)
     for i in new_array:
         summ += i
     return summ
 
 
-
+new_list = []
 print(sum([[1, 2, [3]], [1], 3]))
 print(sum(1, 2, 3, 4, 5))
