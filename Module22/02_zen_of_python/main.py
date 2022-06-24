59dd13737aefca902605bba678fa5e2d878b29b1
zenakmodul14 <zenakorneev940@gmail.com>
2022-06-24 13:48:23 +0300

    2 задание готово

--- Module22/02_zen_of_python/main.py
+++ Module22/02_zen_of_python/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+list_str = list()
+
+speakers_file = open('zen.txt', 'r')
+for element in speakers_file:
+    list_str.append(element)
+speakers_file.close()
+
+for i in list_str[::-1]:
+    print(i, end='')
