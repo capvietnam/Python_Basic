d71bd9a852b4bce6f8df95286c7e6603de7b784a
zenakmodul14 <zenakorneev940@gmail.com>
2022-06-28 22:39:49 +0300

    1 задание готово

--- Module23/01_names_2/main.py
+++ Module23/01_names_2/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+total_symbol = 0
+number = 1
+
+with open('people.txt', 'r', encoding='utf-8') as people:
+    for line in people.readlines():
+        len_lines = len(line) -1
+        try:
+            if len(line)-1 < 4:
+                raise BaseException
+        except BaseException:
+            print(f'менее трёх символов в строке {number}.')
+        total_symbol += len(line)-1
+        number += 1
+
+print(total_symbol)
