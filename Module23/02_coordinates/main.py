b6fa16ab476f3bd863adb705a39c8bca82350eb2
zenakmodul14 <zenakorneev940@gmail.com>
2022-06-29 00:02:54 +0300

    2 задание готово

--- Module23/02_coordinates/main.py
+++ Module23/02_coordinates/main.py
@@ -13,27 +13,41 @@ def f2(x, y):
     return y / x
 
 
+# try:
+#     file = open('coordinates.txt', 'r')
+#     for line in file:
+#         nums_list = line.split()
+#         res1 = f(int(nums_list[0]), int(nums_list[1]))
+#         try:
+#             res2 = f2(int(nums_list[0]), int(nums_list[1]))
+#             try:
+#                 number = random.randint(0, 100)
+#                 file_2 = open('result.txt', 'w')
+#                 my_list = sorted([res1, res2, number])
+#                 file_2.write(' '.join(my_list))
+#             except Exception:
+#                 print("Что-то пошло не так")
+#         except Exception:
+#             print("Что-то пошло не так со второй функцией")
+#         finally:
+#             file.close()
+#             file_2.close()
+# except Exception:
+#     print("Что-то пошло не так с первой функцией")
+
+
 try:
     file = open('coordinates.txt', 'r')
-    for line in file:
+    file_2 = open('result.txt', 'w')
+    for line in file.readlines():
         nums_list = line.split()
         res1 = f(int(nums_list[0]), int(nums_list[1]))
-        try:
-            res2 = f2(int(nums_list[0]), int(nums_list[1]))
-            try:
-                number = random.randint(0, 100)
-                file_2 = open('result.txt', 'w')
-                my_list = sorted([res1, res2, number])
-                file_2.write(' '.join(my_list))
-            except Exception:
-                print("Что-то пошло не так")
-        except Exception:
-            print("Что-то пошло не так со второй функцией")
-        finally:
-            file.close()
-            file_2.close()
+        res2 = f2(int(nums_list[0]), int(nums_list[1]))
+        number = random.randint(0, 100)
+        my_list = sorted([str(res1), str(res2), str(number)])
+        file_2.write(' '.join(my_list) + '\n')
 except Exception:
-    print("Что-то пошло не так с первой функцией")
-
-
-# TODO отредактировать и исправить программу
+    print("Что-то пошло не так")
+finally:
+    file.close()
+    file_2.close()
