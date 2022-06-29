c7d63a9fc5aa6ad9c9147492b8a2b29196fed6fd
zenakmodul14 <zenakorneev940@gmail.com>
2022-06-29 10:30:23 +0300

    3 задание готово

--- Module23/03_lucky_number/main.py
+++ Module23/03_lucky_number/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+from random import randint
+
+
+ex = ['Syntax Errors', 'Exceptions', ]
+summ = 0
+with open('out_file.txt', 'w') as out_file:
+    try:
+        while summ < 777:
+            new_number = input('Введите число: ')
+            if randint(0, 13) == 0:
+                raise Exception
+            out_file.write(new_number + '\n')
+            summ += int(new_number)
+    except Exception:
+        print('Вас постигла неудача!')
+
