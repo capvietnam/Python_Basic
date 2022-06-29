99585e2a09604fa02cbaccd129743c2e847c8754
zenakmodul14 <zenakorneev940@gmail.com>
2022-06-29 14:00:04 +0300

    6 задание готово

--- Module23/06_chat/main.py
+++ Module23/06_chat/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+with open('messenger.txt', '2', encoding='utf-8') as file:
+    pass
+
+while True:
+    name = input('Введите имя: ')
+    action = input('1 - Посмотреть текущий текст чата.\n2 - Отправить сообщение\n1 или 2: ')
+    if action == '2':
+        with open('messenger.txt', 'a', encoding='utf-8') as file:
+            file.write(name + ': ' + input('Ввведите текст')+ '\n')
+    elif action == '1':
+        with open('messenger.txt', 'r', encoding='utf-8') as file:
+            print(file.read())
