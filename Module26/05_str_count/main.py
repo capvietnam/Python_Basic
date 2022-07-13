ccbeb919c58d0fe708cc20c7c1924b2812fc3bb7
zenakmodul14 <zenakorneev940@gmail.com>
2022-07-13 10:55:33 +0300

    5 задание готово

--- Module26/05_str_count/main.py
+++ Module26/05_str_count/main.py
@@ -1,1 +1,30 @@
-# TODO здесь писать код
+import os
+from collections.abc import Iterable
+print(os.path.abspath('main.py'))
+
+def gen_files_dir(path: str) -> Iterable[str]:
+    local_line = 0
+    for root, dirs, files in os.walk(path):
+        for File in files:
+            if File.endswith('.py'):
+                try:
+                    with open(File, 'r') as file:
+                        local_line = 0
+                        for line in file.readlines():
+                            if  line != '\n' and not line.startswith('#'):
+                                local_line += 1
+                except FileNotFoundError:
+                    print('файл уже открыт')
+                yield local_line
+
+i = gen_files_dir('C:\\Users\\zenak\\Desktop\\projects\\pythonProject\\Python_Basic')
+all_line = 0
+for local_line in i:
+    all_line += local_line
+
+
+
+print(all_line)
+
+
+
