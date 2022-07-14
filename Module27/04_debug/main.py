a20e346a37d01d225436eec37b56b925bf6edd65
zenakmodul14 <zenakorneev940@gmail.com>
2022-07-15 02:42:38 +0300

    4 задание готово

--- Module27/04_debug/main.py
+++ Module27/04_debug/main.py
@@ -1,1 +1,30 @@
-# TODO здесь писать код
+from typing import Any, Callable
+
+
+def loging(func) -> Callable:
+    """"декоратор, выводящий имя и значения функции"""
+    name_func = func.__name__
+    def wrapped_func(*args, **kwargs) -> Any:
+        print("Вызывается {}({})".format(
+            func.__name__,
+            ", ".join(list(str(arg) for arg in args)+
+                list('='.join((str(k), str(v))) for k, v in kwargs.items())
+                      )
+        ))
+        result = func(*args, **kwargs)
+        print(f'{name_func} вернула значение {result}')
+        print(result)
+        return result
+    return wrapped_func
+
+@loging
+def greeting(name: str, age: int = None) -> str:
+    if age:
+        return "Ого, {name}! Тебе уже {age} лет, ты быстро растёшь!".format(name=name, age=age)
+    else:
+        return "Привет, {name}!".format(name=name)
+
+
+greeting("Том")
+greeting("Миша", age=100)
+greeting(name="Катя", age=16)
