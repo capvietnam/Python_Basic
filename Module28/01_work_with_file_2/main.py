736dee98253a477c64cce7f9e3ae2219855b5031
zenakmodul14 <zenakorneev940@gmail.com>
2022-07-17 13:47:32 +0300

    1 задание готово

--- Module28/01_work_with_file_2/main.py
+++ Module28/01_work_with_file_2/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+class File:
+    def __init__(self, filename: str, mode: str) -> None:
+        self.filename = filename
+        self.mode = mode
+        self.file = None
+
+    def __enter__(self):
+        try:
+            self.file = open(self.filename, self.mode)
+        except FileNotFoundError:
+            self.mode = 'w+'
+            self.file = open(self.filename, self.mode)
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        self.file.close()
+
+
+with File('rtr.txt', 'w+') as rtr:
+    rtr.write('ghhg')
+
