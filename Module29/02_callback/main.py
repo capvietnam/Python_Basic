62506d5d9dd753366231cb0ec4cea3695405ac47
zenakmodul14 <zenakorneev940@gmail.com>
2022-07-23 15:39:20 +0300

    2 задание готово

--- Module29/02_callback/main.py
+++ Module29/02_callback/main.py
@@ -1,1 +1,34 @@
-# TODO здесь писать код
+from collections.abc import Callable
+import functools
+
+
+app = {}
+
+
+def callback(route: str) -> Callable:
+
+    def wrapped(func: Callable) -> Callable:
+        app[route] = func
+        @functools.wraps(func)
+        def wrapper():
+            ret = func()
+            return ret
+
+        return wrapper
+
+    return wrapped
+
+
+@callback('//')
+def example():
+    print('Пример функции, которая возвращает ответ сервера')
+    return 'OK'
+
+
+route = app.get('//')
+if route:
+    response = route()
+    print('Ответ:', response)
+else:
+    print('Такого пути нет')
+
