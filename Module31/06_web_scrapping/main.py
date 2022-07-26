ea7cea81a89845df96d5dd9006956155cfb0c179
zenakmodul14 <zenakorneev940@gmail.com>
2022-07-27 01:09:47 +0300

    6 задание готово

--- Module31/06_web_scrapping/main.py
+++ Module31/06_web_scrapping/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+import requests
+from bs4 import BeautifulSoup
+
+
+res = []
+url = 'http://www.columbia.edu/~fdc/sample.html'
+response = requests.get(url).content
+soup = BeautifulSoup(response, "html.parser")
+
+for link in soup.find_all('h3'):
+    res.append(link.get_text())
+
+print(res)
