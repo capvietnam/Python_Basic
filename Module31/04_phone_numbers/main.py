b1046427ea7dc923ac09a0ea2830072138328a90
zenakmodul14 <zenakorneev940@gmail.com>
2022-07-27 00:09:57 +0300

    4 задание готово

--- Module31/04_phone_numbers/main.py
+++ Module31/04_phone_numbers/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+from re import findall
+
+
+tel_list = ['9999999999', '999999-999', '99999x9999']
+
+tel_patern_1 = r'[8, 9]\d{9}'
+
+for number in tel_list:
+    if findall(tel_patern_1, number):
+        text = 'всё в порядке'
+    else:
+        text = 'не подходит'
+    print(f'{tel_list.index(number) + 1}й номер:', text)
