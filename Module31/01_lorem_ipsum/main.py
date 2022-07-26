c86a0a49984b0b1eb1c9e53010487e0ea2ccfaa8
zenakmodul14 <zenakorneev940@gmail.com>
2022-07-26 22:19:12 +0300

    1 задание готово

--- Module31/01_lorem_ipsum/main.py
+++ Module31/01_lorem_ipsum/main.py
@@ -1,7 +1,12 @@
+from re import findall
+
 text = """ Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
 Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, 
 nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. 
 Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate 
 """
 
-# TODO здесь писать код
+symbol_4_patern = r'\b\w{4}\b'
+
+full_search = findall(symbol_4_patern, text)
+print(full_search)
