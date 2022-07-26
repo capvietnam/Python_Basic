960bc9649795cf82e460cd3c6e6edd67742b4385
zenakmodul14 <zenakorneev940@gmail.com>
2022-07-26 22:29:18 +0300

    2 задание готово

--- Module31/02_registration_marks/main.py
+++ Module31/02_registration_marks/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+from re import findall
+
+text = 'А578ВЕ777 ОР233787 К901МН666 СТ46599 СНИ2929П777 666АМР666'
+
+taxi_patern = r'\b\w{2}\d{3}\d{2,3}\b'
+private_cars_patern = r'\b\w{1}\d{3}\w{2}\d{2,3}\b'
+
+search_taxi = findall(taxi_patern, text)
+private_cars_search = findall(private_cars_patern, text)
+
+print('Список номеров частных автомобилей:', private_cars_search)
+print('Список номеров такси:', search_taxi)
